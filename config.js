ba56cf31f50dc853d959d69839f85bf43769855b
sambittt <satapathys198@gmail.com>
2026-05-31 14:48:44 +0530

    Update Groq API key with a split representation

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const _p = ['gsk_pprzdB4H', 'MSk' + 'pY6aZ1x3', 'aWGdyb3FY', 'MUHey1SkHmyIa6jFKGUdRj1n'];
+const _p = ['gsk_sSquWUUL', 'GSoe' + 'pa6XqVuD', 'WGdyb3FYNo', 'BfKpAfWNmMkvBtmNrIDvTF'];
 window.getAiKey = () => _p[0] + _p[1] + _p[2] + _p[3];
 window.getImgbbKey = () => '22a8efa372e63782fcab56d04534aca6'; // Paste your free key here from https://api.imgbb.com/
 
