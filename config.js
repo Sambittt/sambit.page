7d4383c3e6f08437332d122d913f6236a7060c9c
sambittt <satapathys198@gmail.com>
2026-05-31 14:13:12 +0530

    Security: untrack config.js, add to .gitignore to protect API keys

--- config.js
+++
@@ -1,5 +0,0 @@
-// API Configuration (Masked)
-const _0x4a2b = 'Z3NrX3hndFNqU3BEMXIxNlRhOGFGOXRsV0dkeWJyRllpQjFaUFJCc0xtSHowU1ZTeW5GMjk5Rg==';
-window.getAiKey = () => atob(_0x4a2b);
-window.getImgbbKey = () => '22a8efa372e63782fcab56d04534aca6'; // Paste your free key here from https://api.imgbb.com/
-
